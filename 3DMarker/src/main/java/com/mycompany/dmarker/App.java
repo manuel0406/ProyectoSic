cbf858fde9acf7d66adf887e035ebb7bd9a792fa
Manuel Ascencio <AA21050@ues.edu.sv>
2023-10-17 09:52:44 -0600

    Creando proyecto

---
+++ 3DMarker/src/main/java/com/mycompany/dmarker/App.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.dmarker;
+
+/**
+ *
+ * @author manue
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
